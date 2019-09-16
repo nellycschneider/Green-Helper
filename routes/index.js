e65c8294c937ee0de802295c7f8810db9cb943d5
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-09-16 15:50:38 +0200

    more routing

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const Animals = require("../models/Animals");
 const Enviroment = require("../models/Environment");
+const Recipes = require("../models/Recipes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+//re
 router.get("/dashboard", (req, res) => {
   var myAnimals;
+  // var environment;
 
   Animals.find()
     .then(animals => {
@@ -17,9 +20,24 @@ router.get("/dashboard", (req, res) => {
       myAnimals = animals;
       Enviroment.find()
         .then(env => {
-          console.log("aaaaanimals-----", myAnimals);
+          // console.log("aaaaanimals-----", myAnimals);
+          Recipes.find()
+            .then(recipe => {
+              // console.log("aaaaanimals-----", myAnimals);
+              // console.log("envvvv-----", env);
+              // console.log(recipe);
 
-          res.render("dashboard", { envFacts: env, animalList: myAnimals });
+              res.render("dashboard", {
+                envFacts: env,
+                animalList: myAnimals,
+                recipeList: recipe
+              });
+            })
+            .catch(err => {
+              console.log(err);
+            });
+
+          // res.render("dashboard", { envFacts: env, animalList: myAnimals });
         })
         .catch(err => {
           console.log(err);
@@ -32,8 +50,18 @@ router.get("/dashboard", (req, res) => {
     });
 });
 
-router.get("/dashboard/:animalId", (req, res) => {
-  //
+router.get("/animals", (req, res, next) => {
+
+  currentDate - req.user.created_at
+
+  Animals.findOne({ day: 4 })
+    .then(animal => {
+      //
+      res.render("animals", { animal: animal });
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
