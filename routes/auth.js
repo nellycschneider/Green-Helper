763171f16606d238bdc48e02a7445873fc16727b
Nelly Schneider <nellschge@stadtlengsfeld.net>
2019-09-17 15:20:55 +0200

    added routes, kill counter and styled the homepage

--- routes/auth.js
+++ routes/auth.js
@@ -14,7 +14,7 @@ router.get("/login", (req, res, next) => {
 router.post(
   "/login",
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/dashboard",
     failureRedirect: "/auth/login",
     failureFlash: true,
     passReqToCallback: true
@@ -50,7 +50,7 @@ router.post("/signup", (req, res, next) => {
     newUser
       .save()
       .then(() => {
-        res.redirect("/");
+        res.redirect("/dashboard");
       })
       .catch(err => {
         res.render("auth/signup", { message: "Something went wrong" });
--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ const User = require("../models/User");
 router.get("/", (req, res, next) => {
   const user = req.user;
   console.log("req.user: ", req.user);
-  res.render("index", { user: user });
+  res.render("index", { user: user, layout: false });
 });
 
 // create a middleware that checks if a user is logged in
@@ -41,7 +41,6 @@ router.get("/dashboard", loginCheck(), (req, res) => {
       myAnimals = animals;
       Enviroment.find()
         .then(env => {
-          // console.log("aaaaanimals-----", myAnimals);
           Recipes.find()
             .then(recipe => {
               // console.log("aaaaanimals-----", myAnimals);
@@ -69,7 +68,7 @@ router.get("/dashboard", loginCheck(), (req, res) => {
     });
 });
 
-router.get("/animals", (req, res, next) => {
+router.get("/animals", loginCheck(), (req, res, next) => {
   let today = new Date();
   let currentDay = today.getDate();
   console.log(currentDay);
@@ -79,24 +78,49 @@ router.get("/animals", (req, res, next) => {
   Animals.findOne({ day: displayDate })
     .then(animal => {
       //
-      res.render("animals", { animal: animal });
+      res.render("animals", { animal: animal, layout: false });
     })
     .catch(err => {
       next(err);
     });
 });
 
-router.get("/recipes", (req, res, next) => {
-  // currentDate - req.user.created_at
+router.get("/recipes", loginCheck(), (req, res, next) => {
+  let today = new Date();
+  let currentDay = today.getDate();
+  console.log(currentDay);
+  let user = req.user;
+  let displayDate = currentDay - user.created_at.getDate() + 1;
 
-  Recipes.findOne({day: 1})
+  Recipes.findOne({ day: displayDate })
     .then(recipe => {
       //
-      res.render("recipes",{ recipes: recipe, layout: false });
+      res.render("recipes", { recipes: recipe, layout: false });
     })
     .catch(err => {
       next(err);
     });
+});
+
+router.get("/environment", loginCheck(), (req, res, next) => {
+  let today = new Date();
+  let currentDay = today.getDate();
+  console.log(currentDay);
+  let user = req.user;
+  let displayDate = currentDay - user.created_at.getDate() + 1;
+
+  Enviroment.findOne({ day: displayDate })
+    .then(env => {
+      //
+      res.render("environment", { environment: env, layout: false });
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+router.get("/killcounter", (req, res) => {
+  res.render("killcounter", { layout: false });
 });
 
 module.exports = router;
